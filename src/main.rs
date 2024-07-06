a0866cceebd774ec686f0e40797f568f57c03bc5
ludndev <ludndev@gmail.com>
2024-07-06 09:51:36 +0100

    vendor: install clap crate
    used for cli args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Sets the type of tags to generate (alphanumeric, latin, english, french)
+    #[arg(default_value_t = String::from("latin"))]
+    dict: String,
+
+    /// Sets the number of tags to generate
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    let tag_type = args.dict;
+
+    println!("{}", args.count);
+    println!("{}", tag_type);
 }
